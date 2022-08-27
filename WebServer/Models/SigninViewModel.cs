2597db558d17926fdee8659f47701ccf745a588d
mayfish <maysnk@hotmail.com>
2022-08-27 16:16:38 +0800

    fixed

--- WebServer/Models/SigninViewModel.cs
+++ WebServer/Models/SigninViewModel.cs
@@ -4,8 +4,8 @@ namespace WebServer.Models
 {
     public class SigninViewModel
     {
-        [Display(Name = "帳號")]
-        [Required(ErrorMessage = "帳號必填")]
+        [Display(Name = "Account")]
+        [Required(ErrorMessage = "AccountRequired")]
         public string? Account { get; set; }
         [Display(Name = "密碼")]
         [Required(ErrorMessage = "密碼必填")]
